8331a175d232b4046afda25f09741086c53ba00d
Bayu Arma Praja <100572149+Yuu18id@users.noreply.github.com>
2024-11-05 16:08:09 +0700

    Update model.js

--- models/model.js
+++ models/model.js
@@ -1,9 +1,9 @@
 const {Sequelize, DataTypes} = require('sequelize');
 
-const sequelize = new Sequelize("web_chat", "root", "", {  // db name, username, password
-    host    : 'localhost', 
-    dialect : 'mysql',
-    password: 'root'
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST, // Ini harus 'db'
+    dialect: 'mysql',
+    port: 3306 // Port default MySQL
 });
 
 module.exports = {sequelize, DataTypes}
