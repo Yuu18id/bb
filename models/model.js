5b6565961ee9b900d9cffe4aa62eef8260f41e3b
“Yuu18id“ <211112007@students.mikroskil.ac.id>
2024-11-05 17:14:18 +0700

    fix 6

--- models/model.js
+++ models/model.js
@@ -3,7 +3,7 @@ const {Sequelize, DataTypes} = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST, // Ini seharusnya 'db'
     dialect: 'mysql',
-    port: 3001
+    port: 3306
   });
 
 module.exports = {sequelize, DataTypes}
